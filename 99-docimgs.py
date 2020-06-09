4b2408962f9d0772ea2daa493bf380acd41d0cbb
Mutaz Abu Ghazaleh <mutaz.m.abughazaleh@student.uts.edu.au>
2020-06-09 16:50:33 +1000

    doc images

--- 99-docimgs.py
+++ 99-docimgs.py
@@ -1,19 +1,15 @@
 import os
-import urllib
+from urllib.parse import quote
 urlroot = 'https://raw.githubusercontent.com/mutazag/godzilla-sina/master/docimgs/'
 
 
 root = 'docimgs'
-filepaths = [os.path.join(root,f) for f in  os.listdir(root)]
+filepaths = [os.path.join(root, f) for f in os.listdir(root)]
 # [os.path.basename(f) for f in os.listdir(root)]
 datasets = [os.path.splitext(f)[0] for f in os.listdir(root)]
 
+imgs = [os.path.splitext(f)[0] + ',' + (urlroot + quote(f)) for f in os.listdir(root)]
 
 
-
-imgs = [(urlroot+urllib.parse.quote(f))
-    for f in os.listdir(root) ]
-
-
-for img in imgs: 
+for img in imgs:
     print(img)
